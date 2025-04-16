65c6fd8933d08f3a29cba60d580d4edfa2baa115
Eyal <eyal@pengusystems.com>
2025-04-16 16:34:41 -0600

    Add pulser pynb

---
+++ scripts/build/grpc/compile_pulser_examples.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#This script will resolve the C++ externals drogon and trantor.
+#Operating systems supported:
+# * Ubuntu/Debian/Mint
+# * Windows
+# * OSx (not tested)
+set -e
+
+while getopts h flag
+do
+    case "${flag}" in
+        h) echo "Usage: ${0##*/}" && exit;;
+    esac
+done
+
+echo Compiling examples...
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+repo_root=$(realpath $script_dir/../../..)
+proto_dir=$repo_root/src/pulser/grpc/proto
+dst_dir=$repo_root/build
+
+# Get the system name.
+uname_out="$(uname -s)"
+case "${uname_out}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Windows;;
+    MINGW*)     machine=Windows;;
+    *)          machine="UNKNOWN:${uname_out}"
+esac
+
+# Compile for python.
+python_dst_dir=$dst_dir/pulser/grpc/python
+mkdir -p $python_dst_dir
+cd $python_dst_dir
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install grpcio
+python3 -m pip install grpcio-tools
+python3 -m pip install notebook
+python3 -m pip install ipykernel
+mkdir -p $python_dst_dir/generated
+for proto_file in "$proto_dir"/*.proto; do
+  python3 -m grpc_tools.protoc -Igenerated=$proto_dir -I"$proto_dir" --python_out="$python_dst_dir" --pyi_out="$python_dst_dir" --grpc_python_out="$python_dst_dir" "$proto_file"
+done
+ln -f "$repo_root"/src/pulser/grpc/python/pulser_grpc.ipynb pulser_grpc.ipynb
+
+echo Completed compiling examples
+echo ** To see the python examples run: source $repo_root/scripts/setup/set_env.sh ; pulser-run-grpc-example-pynb
---
+++ scripts/setup/set_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#This script will setup the environment for development.
+#Use "source ./set_env_development.sh" to source.
+#Include in ~/.bashrc for convenience.
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Environment variables.
+repo_root=$(realpath $script_dir/../..)
+export GREENLAND_ROOT=$repo_root
+export GREENLAND_SCRIPTS=$repo_root/scripts
+
+# Custom commands.
+pulser-run-grpc-example-pynb() {
+    cd $repo_root/build/pulser/grpc/python
+    source .venv/bin/activate
+    jupyter notebook --ip='*' --no-browser --port=9999
+    deactivate
+}
+
+# Allow forward search (i-search)
+stty -ixon
+
+# Workaround to enable tab autocomplete with environment variables.
+shopt -s direxpand
+
+# Increase limit for core dumps (on ubuntu those will appear under /var/lib/apport/coredump/).
+ulimit -c unlimited
+
+# A new shell gets the history lines from all previous shells.
+PROMPT_COMMAND='history -a'
