d019023c945182948abd0dd1f6b2ed9ca73b1f7e
Eyal <eyal@pengusystems.com>
2025-05-06 18:40:21 -0600

    More mini-tdr skeleton

---
+++ scripts/linux/build/grpc/compile_pulser_examples.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#This script will compile the pulser examples for Linux.
+#Operating systems supported:
+# * Ubuntu/Debian/Mint
+set -e
+while getopts h flag
+do
+    case "${flag}" in
+        h) echo "Usage: ${0##*/}" && exit;;
+    esac
+done
+
+echo Compiling examples...
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+repo_root=$(realpath $script_dir/../../../..)
+proto_dir=$repo_root/src/pulser/grpc/proto
+dst_dir=$repo_root/build
+
+# Get the system name.
+uname_out="$(uname -s)"
+case "${uname_out}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Windows;;
+    MINGW*)     machine=Windows;;
+    *)          machine="UNKNOWN:${uname_out}"
+esac
+
+# Compile for python.
+if [[ $machine != "Linux" ]]; then
+    echo "This script is only supported in Linux"
+    exit 1
+fi
+if ! command -v python3 >/dev/null 2>&1; then  # needed for running grpc from cli.
+    echo Please install Python
+    exit 1
+elif python3 -c "import sys; exit(sys.version_info >= (3, 10))"; then
+    echo "Python version installed is $(python3 -V | awk -F' ' '{print $2}'). Version 3.10 or newer is required!"
+    exit 1
+fi
+python_dst_dir=$dst_dir/pulser/grpc/python
+mkdir -p $python_dst_dir
+cd $python_dst_dir
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install grpcio
+python3 -m pip install grpcio-tools
+python3 -m pip install notebook
+python3 -m pip install ipykernel
+mkdir -p $python_dst_dir/generated
+for proto_file in "$proto_dir"/*.proto; do
+  python3 -m grpc_tools.protoc -Igenerated=$proto_dir -I"$proto_dir" --python_out="$python_dst_dir" --pyi_out="$python_dst_dir" --grpc_python_out="$python_dst_dir" "$proto_file"
+done
+for pynb_file in "$repo_root"/src/pulser/grpc/python/*.ipynb; do
+    pynb_basename=$(basename $pynb_file)
+    ln -f "$pynb_file" "$pynb_basename"
+done
+
+echo Completed compiling examples
+echo "** To see the python examples run: source $repo_root/scripts/linux/setup/set_env.sh ; pulser-run-grpc-example-pynb **"
---
+++ scripts/windows/run/mini-tdr-run-grpc-example-pynb.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+REM Run notebook.
+set "script_dir=%~dp0"
+if "%script_dir:~-1%"=="\" set "script_dir=%script_dir:~0,-1%"
+for %%i in ("%script_dir%\..\..\..") do set "repo_root=%%~fi"
+cd %repo_root%\build\mini-tdr\grpc\python
+call .venv\Scripts\activate.bat
+jupyter notebook --ip='*' --no-browser --port=9999
